5b5d9dd027e81fae32b517b93c4a4f6e7340882f
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-08-24 20:41:37 -0300

    Refactor home page to use CSS modules for styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
-    <>
-     <div>
-    <h1>Hello World</h1>
-  </div>
-    </>
-  )
- 
+      <div>
+        <div className={styles.container_principal}>
+          <div className={styles.container_texto}>
+            <h1>Hello World</h1>
+            <p>paragrafo com react</p>
+            <p className={styles.autor}>Criado por DevMedia</p>
+          </div>
+        </div>
+        <div/>
+      </div>
+    );
 }
